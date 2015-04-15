68d5c796f92d1d97b8b82d8ce7f9cc0138233e90
Chris Vickery <c4@swirl.com>
2015-04-15 17:51:24 -0400

    Added pmx remote commands

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 var Maki = require('maki');
 var app = new Maki(require('../config'));
+var pmx = require('pmx');
+
+pmx.action('npm:install', function(reply) {
+	require('child_process').exec('npm install', function (error, stdout, stderr) {
+		if (error !== null) {
+			reply("Error: " + stderr);
+		} else {
+			reply("Done: " + stdout);
+		}
+	});
+});
 
 app.start();
 
