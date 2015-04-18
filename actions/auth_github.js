81f5ad04ef96262197b9986eb89951e112d02bed
Chris Vickery <c4@swirl.com>
2015-04-18 11:02:07 -0400

    Update to callback

--- actions/auth_github.js
+++ actions/auth_github.js
@@ -12,10 +12,13 @@ exports.auth_github = {
 	outputExample: {
 	},
 	run: function(api, connection, next) {
-		// api.log(connection);
+		api.log(connection.params);
+		// next(connection, true);
 		passport.authorize('github', {
 			failureRedirect: '/login'
 		})(connection.rawConnection.req, connection.rawConnection.res, function(err) {
+			api.log("This is the callback for the github authorize");
+			api.log(arguments);
 			if (err instanceof Error) {
 				connection.error = err;
 			}
