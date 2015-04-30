c2de857e05f5cfcd79fc57474474f632775644b2
Chris Vickery <c4@swirl.com>
2015-04-29 23:04:41 -0400

    Forgot to add this file before

---
+++ models/match.js
@@ -0,0 +1,29 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var Types = mongoose.Types;
+
+var matchSchema = mongoose.Schema({
+	players: [{
+		position: 	{ type: String , enum: [ 'defense', 'offense', 'mixed' ] , required: true },
+		player: 	{ type: Schema.Types.ObjectId , required: true },
+		mmr: 		{ type: Number , required: true },
+		team: 		{ type: Number , min: 0 , max: 1 , required: true },
+		winner: 	{ type: Number , min: 0 , max: 1 }
+	}],
+
+	type: 		{ type: String , enum: [ '1v1', '2v2', 'foosbeer' ] },
+
+	winner: 	{ type: Number , min: 0 , max: 1 }
+});
+
+// matchSchema.virtual('full_name').get(function () {
+// 	return this.first_name + ' ' + this.last_name;
+// });
+
+// Create the mongoose model
+var _model = mongoose.model('Match', matchSchema);
+
+module.exports = {    
+	schema : matchSchema,
+	model : _model
+};
